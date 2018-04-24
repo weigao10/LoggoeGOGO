e0e7991b4f5e88422ea20224593656ec3dc0337e
Tom Wagner <wagne868@umn.edu>
2018-04-24 17:07:51 -0400

    Working instantiation of database tables

--- schema.sql
+++ schema.sql
@@ -6,57 +6,84 @@ USE oneTeam;
 CREATE TABLE users (
   -- 'name' is equivalent to username
   -- not renamed to username in order to avoid breaking legacy code
-  id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+
+  -- LEGACY CODE:
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name varchar(255) NOT NULL UNIQUE KEY,
+  owner BOOLEAN NOT NULL, -- 1 if user is a teacher, 0 if user is a student
+
+  -- NEW COLUMNS:
   firstName varchar(255),
   lastName varchar(255),
   hashedPassword varchar(255),
-  salt varchar(255),
-  owner BOOLEAN NOT NULL -- 1 if user is a teacher, 0 if user is a student
+  salt varchar(255)
 );
 
 CREATE TABLE videos (
-  id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  -- LEGACY CODE:
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   videoId varchar(255) NOT NULL UNIQUE KEY,
   title varchar(255) NOT NULL,
   description varchar(255),
   image varchar(255),
-  userId INT(11) NOT NULL,
+  userId INT NOT NULL, -- changed from INT(11) to INT in order to set as Foreign Key
   duration INT(11),
+
+  -- NEW COLUMNS:
   videoDescription varchar(255),
   series varchar(255),
-  idxInSeries INT(11),
+  idxInSeries INT,
 
+  -- FOREIGN KEY REFERENCES TO OTHER TABLES:
   FOREIGN KEY (userId) REFERENCES users(id)
 );
 
-CREATE TABLE studentComments (
-  id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  videoId INT(11) NOT NULL,
+CREATE TABLE timeStamps (
+  -- LEGACY CODE:
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  videoId varchar(255) NOT NULL,
+  userId INT NOT NULL, -- changed from INT(11) to INT in order to set as Foreign Key
   timeStamp INT(11) NOT NULL,
-  userId INT(11) NOT NULL,
+  comment varchar(255),
+
+  -- NEW COLUMNS:
+  addressedByTeacher BOOLEAN NOT NULL DEFAULT 0,
+  commentType varchar(255),
+  video INT NOT NULL,
+
+  -- FOREIGN KEY REFERENCES TO OTHER TABLES:
+  FOREIGN KEY (userId) REFERENCES users(id),
+  FOREIGN KEY (video) REFERENCES videos(id)
+);
+
+-- NEW TABLE:
+CREATE TABLE teacherComments (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  comment varchar(255),
+  video INT NOT NULL,
+  userId INT NOT NULL,
+  begRange INT, -- Beginning timestamp of the video addressed by comment
+  endRange INT, -- Ending timestamp of the video addressed by comment
+  commentType varchar(255),
+
+  FOREIGN KEY (userId) REFERENCES users(id),
+  FOREIGN KEY (video) REFERENCES videos(id)
+);
+
+-- NEW TABLE:
+CREATE TABLE studentComments (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  video INT NOT NULL,
+  timeStamp INT NOT NULL,
+  userId INT NOT NULL,
   comment varchar(255),
   addressedByTeacher BOOLEAN NOT NULL DEFAULT 0,
   commentType varchar(255),
 
   FOREIGN KEY (userId) REFERENCES users(id),
-  FOREIGN KEY (videoId) REFERENCES videos(id)
+  FOREIGN KEY (video) REFERENCES videos(id)
 );
 
-CREATE TABLE teacherComments (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  comment varchar(255),
-  videoId INT(11) NOT NULL,
-  userId INT(11) NOT NULL,
-  begRange INT(11), -- Beginning timestamp of the video addressed by comment
-  endRange INT(11), -- Ending timestamp of the video addressed by comment
-  commentType varchar(255),
-
-  FOREIGN KEY (userId) REFERENCES users(id),
-  FOREIGN KEY (videoId) REFERENCES videos(id)
-);
-
-INSERT IGNORE INTO users (name, owner) VALUES ('jun yoo', true);
 INSERT IGNORE INTO users (name, firstName, lastName, hashedPassword, salt, owner) VALUES ('tom', 'Tom', 'Wagner', '123', '456', true);
 INSERT IGNORE INTO users (name, firstName, lastName, hashedPassword, salt, owner) VALUES ('amy', 'Amy', 'San Felipe', '123', '456', true);
 INSERT IGNORE INTO users (name, firstName, lastName, hashedPassword, salt, owner) VALUES ('ian', 'Ian', 'Pradhan', '123', '456', false);
@@ -64,5 +91,5 @@ INSERT IGNORE INTO users (name, firstName, lastName, hashedPassword, salt, owner
 
 INSERT IGNORE INTO videos (videoId, title, userId, description, image, duration) VALUES ('ZK3O402wf1c', 'Lec 1 | MIT 18.06 Linear Algebra, Spring 2005', 1, 'Lecture 1: The Geometry of Linear Equations. View …e information at http://ocw.mit.edu/terms More...', 'https://i.ytimg.com/vi/ZK3O402wf1c/default.jpg', 2389);
 
-INSERT INTO studentComments (videoId, userId, timeStamp, comment) VALUES (1, 4, 132, 'hello so confused!!');
-INSERT INTO teacherComments (videoId, userId, begRange, endRange, comment) VALUES (1, 2, 110, 132, "I'm sorry you're confused!");
+INSERT INTO studentComments (video, userId, timeStamp, comment) VALUES (1, 4, 132, 'hello so confused!!');
+INSERT INTO teacherComments (video, userId, begRange, endRange, comment) VALUES (1, 2, 110, 132, "I'm sorry you're confused!");
