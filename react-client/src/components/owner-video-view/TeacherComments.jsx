14d07d10ee5c4176ad9af94339dfae30f5abf2a6
Wei Gao <wei.gao42@gmail.com>
2018-04-26 12:27:37 -0400

    adds upload button and ability to select uploads

--- react-client/src/components/owner-video-view/TeacherComments.jsx
+++ react-client/src/components/owner-video-view/TeacherComments.jsx
@@ -1,14 +1,49 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+import filestack from 'filestack-js';
+
+const apikey = 'A28J1LUkTB6r5Y2TZwcKoz';
 
 class TeacherComments extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+
+    this.openPicker = this.openPicker.bind(this);
+  }
+
+  componentDidMount() {}
 
   render() {
-    return(
+    return (
       <Paper>
-        <div>Teacher Comments Box</div>
+        <RaisedButton 
+          label="Upload File"
+          onClick={this.openPicker} />
       </Paper>
-    )
+    );
+  }
+
+  openPicker() {
+    var fsClient = filestack.init("A28J1LUkTB6r5Y2TZwcKoz");
+    fsClient
+      .pick({
+        fromSources: [
+          "local_file_system",
+          "imagesearch",
+          "googledrive",
+          "dropbox",
+          "evernote",
+          "github"
+        ]
+      })
+      .then(function(response) {
+        // declare this function to handle response
+        // handleFilestack(response);
+        console.log("heyyyyyy");
+      });
   }
 }
 
