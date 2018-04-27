a779f7106995c8807fad2e32bc9d13000e034a6b
Amy San Felipe <amysanfelipe@gmail.com>
2018-04-27 16:18:58 -0400

    slider shows times as you slide it

--- react-client/src/components/owner-video-view/CommentSlider.jsx
+++ react-client/src/components/owner-video-view/CommentSlider.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import Slider, {Range} from 'rc-slider';
 import TeacherForm from './TeacherForm.jsx';
+import moment from 'moment';
 
 class CommentSlider extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      start: 0,
-      end: 0
+      start: '00:00',
+      end: '00.00'
     }
+    this.onSliderChange = this.onSliderChange.bind(this);
+    this.convertTime = this.convertTime.bind(this);
   }
 
   onSliderChange = (value) => {
-    console.log(value);
     this.setState({
-      start: value[0],
-      end: value[1]
+      start: this.convertTime(value[0]),
+      end: this.convertTime(value[1])
     })
   }
 
+  convertTime(time) {
+    let output = time >= 3600 ? moment.utc(time*1000).format('HH:mm:ss') : moment.utc(time*1000).format('mm:ss');
+    return output;
+  }
 
   render() {
     return(
@@ -26,6 +32,7 @@ class CommentSlider extends React.Component {
         <div style={{marginBottom: '10px', paddingTop: '10px'}}>Set time range:</div>
         <div style={style}>
           <Range allowCross={false} defaultValue={[0, 0]} min={0} max={this.props.video.duration} onChange={this.onSliderChange} />
+          <div>Start: {this.state.start} End: {this.state.end}</div>
         </div>
         <div>
           <TeacherForm save={this.props.save} video={this.props.video} start={this.state.start} end={this.state.end}/>
